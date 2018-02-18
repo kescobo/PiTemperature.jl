4bca5ce53b11912d1d8f02999af9cb429e32f862
Kevin Bonham <kevbonham@gmail.com>
2018-02-18 06:38:20 -0500

    fix function

--- src/tempsensor.jl
+++ src/tempsensor.jl
@@ -2,14 +2,14 @@
 Get the temperature from a particular sensor based on unique id
 """
 function readtemp(sensorid::String, sensordir::String="/sys/bus/w1/devices")
-    sid = joinpath(sensors, sensorid)
+    sid = joinpath(sensordir, sensorid)
     p = joinpath(sid, "w1_slave")
     isfile(p) || error("Couldn't find sensor $sensorid at '/sys/bus/w1/devices'")
 
     readings = readlines(p)
     if endswith(readings[1], "YES")
-        m = match(r"t=(\d+)$", s)
-        ctemp = parse(m.captures[1], Float64) / 1000
+        m = match(r"t=(\d+)$", readings[2])
+        ctemp = parse(Float64, m.captures[1]) / 1000
         ctemp == 85. && warn("Sensor $sensorid may be improperly connected (temp = 85.000°C)")
         ftemp = ctemp * 1.8 + 32
 
