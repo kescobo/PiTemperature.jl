ec186d4a735722cbc388de1afa14a2001381f364
Kevin Bonham <kevbonham@gmail.com>
2018-04-28 08:50:44 -0400

    minimal

--- src/PiTemperature.jl
+++ src/PiTemperature.jl
@@ -2,15 +2,8 @@ module PiTemperature
 
 export
     sensorids,
-    readtemp,
-#    temprange,
-#    temprange!
+    readtemp
 
-import PiGPIO
-#import Unitful.DefaultSymbols
-#import Base.convert
-
-#include("tempcontrol.jl")
 include("tempsensor.jl")
 
 end # module
